47ffe29f20a6702d4b41f1fc8c8087af379d38f1
Suraj Sharma <suraj@evenzero.in>
2024-02-28 17:59:39 +0530

    feat: init

---
+++ App.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+
+export default function App() {
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+
+  const subscribe = async () => {
+    const isAvailable = await Pedometer.isAvailableAsync();
+    setIsPedometerAvailable(String(isAvailable));
+
+    const permission = await Pedometer.requestPermissionsAsync();
+
+    console.log("Permision", permission);
+
+    if (isAvailable) {
+      return Pedometer.watchStepCount(result => {
+        if(permission.granted)
+        setCurrentStepCount(result.steps);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const subscription = subscribe();
+
+    return () => subscription && subscription.remove();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
+      <Text>Walk! And watch this go up: {currentStepCount}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
